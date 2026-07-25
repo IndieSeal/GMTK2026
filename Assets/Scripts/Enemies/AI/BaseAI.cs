d67ff83c9898678fadbbb36f8ffa716804b9b08e
BBM <77977795+BoomBoomMushroom@users.noreply.github.com>
2026-07-24 22:28:10 -0400

    feat: some code for enemies, need to make actual shooting velocity vectors now

--- Assets/Scripts/Enemies/AI/BaseAI.cs
+++ Assets/Scripts/Enemies/AI/BaseAI.cs
@@ -1,18 +1,44 @@
 using UnityEngine;
+using uPools;
 
 public class BaseAI : MonoBehaviour
 {
-    SpriteRenderer spriteRenderer;
+    public SpriteRenderer spriteRenderer;
+    public GameObject attackProjectile; // if left null then the enemy will be treated as a melee enemy
+
+    // Shooting
+    float attackCooldown = 0f;
+    float currentAttackCooldown = 2f;
+    float bulletVelocity = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Start()
     {
-        
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
+        currentAttackCooldown -= Time.deltaTime;
+    }
+
+    public bool canShoot()
+    {
+        if(currentAttackCooldown > 0){ return false; } // on cooldown
+
+        return true;
+    }
+
+    public void Shoot(Vector3 targetPosition)
+    {
+        if(canShoot() == false){ return; }
+        currentAttackCooldown = attackCooldown;
+
+        GameObject bulletInstance = SharedGameObjectPool.Rent(attackProjectile, transform.position, Quaternion.identity);
         
+        if(bulletInstance.TryGetComponent(out Rigidbody2D rb)){
+            rb.linearVelocity = Vector2.zero.normalized * bulletVelocity;
+        }
     }
 }
--- Assets/Scripts/Enemies/AI/DisappearingEnemy.cs
+++ Assets/Scripts/Enemies/AI/DisappearingEnemy.cs
@@ -1,16 +1,40 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class DisappearingEnemy : BaseAI
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float reappearTelegraphTime = 0.5f;
+
+    private IEnumerator HandleReapearTelegraphDelay()
     {
-        
+        yield return new WaitForSeconds(reappearTelegraphTime);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public new void Start()
+    {
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    public new void Update()
     {
-        
+        base.Update();
+        Shoot();
+    }
+
+
+    // just keep the new keyword b/c it doesn't break anything
+    public new void Shoot()
+    {
+        Color spriteColor = base.spriteRenderer.color;
+        spriteColor.a = 1; // visible
+        base.spriteRenderer.color = spriteColor;
+        StartCoroutine(HandleReapearTelegraphDelay()); // wait a sec after reappearing
+        base.Shoot( new Vector3(0,0,0) );
+        StartCoroutine(HandleReapearTelegraphDelay()); // wait a sec after shooting
+        spriteColor.a = 0; // transparent
+        base.spriteRenderer.color = spriteColor;
     }
 }
